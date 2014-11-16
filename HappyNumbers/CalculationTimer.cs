7a0bb51f2cd5c47b111f026d4ce9616e99ab1875
Paul D'Ambra <pdambra@roishoppermarketing.com>
2014-11-16 10:33:56 +0000

    final form

---
+++ HappyNumbers/CalculationTimer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HappyNumbers
+{
+    class CalculationTimer
+    {
+        [Test]
+        public void RunForFiveSeconds()
+        {
+            var maximums = new List<int>(20);
+            for (var i = 0; i < 20; i++)
+            {
+                maximums.Add(GetMaxReachedInFiveSeconds());
+            }
+            Debug.WriteLine("In five seconds the highest number checked was {0}", maximums.Average());
+        }
+
+        [Test]
+        public void ParallelRunForFiveSeconds()
+        {
+            var maximums = new List<int>(20);
+
+            Parallel.For(0, 200, i =>
+            {
+                maximums.Add(GetMaxReachedInFiveSeconds()); 
+                HappyNumbers.ResetHappyNumberRecords();
+            });
+
+            Debug.WriteLine("In five seconds the highest number checked was {0}", maximums.Average());
+        }
+
+        private static int GetMaxReachedInFiveSeconds()
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var numberReached = 0;
+            while (stopwatch.Elapsed.Seconds < 5)
+            {
+                numberReached++.IsAHappyNumber();
+            }
+            return numberReached;
+        }
+    }
+}
---
+++ HappyNumbers/HappyNumbers.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace HappyNumbers
+{
+    // Choose a two-digit number (eg. 23), square each digit and add them together. 
+    // Keep repeating this until you reach 1 or the cycle carries on in a continuous loop. 
+    // If you reach 1 then the number you started with is a “happy number”.
+    public static class HappyNumbers
+    {
+        private static readonly Dictionary<int, HashSet<int>> NumberChain = new Dictionary<int, HashSet<int>>(); 
+        private static readonly Dictionary<string, bool> HappyNumberResults = new Dictionary<string, bool>();
+
+        public static bool IsAHappyNumber(this int startingNumber)
+        {
+            var digitsToTest = startingNumber.GetDigits().StripZeroes().OrderedByDigit();
+            var happyNumberKey = string.Join(",", digitsToTest);
+
+            if (HappyNumberResults.ContainsKey(happyNumberKey))
+            {
+                return HappyNumberResults[happyNumberKey];
+            }
+
+            if (NumberChain.ContainsKey(startingNumber))
+            {
+                throw new Exception(
+                    string.Format(
+                        "I didn't think we could have a number ({0}) without a result that was in the number chain at this point",
+                        startingNumber));
+            }
+
+            NumberChain.Add(startingNumber, new HashSet<int>{startingNumber});
+
+            TestForHappiness(startingNumber, happyNumberKey);
+
+            return HappyNumberResults[happyNumberKey];
+        }
+
+        private static void TestForHappiness(int startingNumber, string happyNumberKey)
+        {
+            var nextInChain = startingNumber;
+            while (!HappyNumberCalculationIsCompleteFor(happyNumberKey))
+            {
+                nextInChain = nextInChain.GetSumOfSquaredDigits();
+                if (nextInChain == 1)
+                {
+                    HappyNumberResults.Add(happyNumberKey, true);
+                    break;
+                }
+                if (TheCalculationChainHasLoopedAround(startingNumber, nextInChain))
+                {
+                    HappyNumberResults.Add(happyNumberKey, false);
+                    break;
+                }
+            }
+        }
+
+        public static void ResetHappyNumberRecords()
+        {
+            NumberChain.Clear();
+            HappyNumberResults.Clear();
+        }
+
+        /// <summary>
+        /// If the last calculated sum of the squares of the digits is already in the set of calculated numbers
+        /// then this number chain has looped around
+        /// </summary>
+        private static bool TheCalculationChainHasLoopedAround(int startingNumber, int sumOfSquaredDigits)
+        {
+            var canAddToTheNumbersInThisChain = NumberChain[startingNumber].Add(sumOfSquaredDigits);
+            return !canAddToTheNumbersInThisChain;
+        }
+
+        private static bool HappyNumberCalculationIsCompleteFor(string happyNumberKey)
+        {
+            return HappyNumberResults.ContainsKey(happyNumberKey);
+        }
+
+        private static int GetSumOfSquaredDigits(this int number)
+        {
+            return number.GetDigits()
+                         .Sum(digit => digit*digit);
+        }
+
+        public static IEnumerable<int> GetDigits(this int number)
+        {
+            return number.ToString(CultureInfo.InvariantCulture)
+                         .Select(digit => int.Parse(digit.ToString(CultureInfo.InvariantCulture)));
+        }
+
+        public static IEnumerable<int> StripZeroes(this IEnumerable<int> numbers)
+        {
+            return numbers.Where(digit => digit != 0);
+        } 
+
+        public static IEnumerable<int> OrderedByDigit(this IEnumerable<int> numbers)
+        {
+            return numbers.OrderBy(i=>i);
+        }
+    }
+}
